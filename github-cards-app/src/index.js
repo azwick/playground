c8295844b80eef82b32b7dc7ac3bb0fe8b69d962
Mrs. Zwickermann <azimmermann@epages.com>
2021-01-15 20:52:02 +0100

    Update: Second try to rewrite classes into functions

--- github-cards-app/src/index.js
+++ github-cards-app/src/index.js
@@ -1,67 +1,96 @@
 import './index.css';
 
-// https://reactjs.org/docs/hooks-state.html
 import React, { useState } from 'react';
 
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-// Convert an array of records into an array of card components
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
 const CardList = (props) => (
   <div>
-    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+    {props.profiles.map(profile => <Card key={profile.id} {...profile} />)}
   </div>
 );
 
-// Render information about a gitHub profile
-const Card = (props) => (
+const Card = function(props) {
+  const profile = props;
+
+  return (
     <div className="github-profile">
-      <img src={props.avatar_url} alt={props.name} />
+      <img src={profile.avatar_url} alt="" />
       <div className="info">
-        <div className="name">{props.name}</div>
-        <div className="company">{props.company}</div>
+        <div className="name">{profile.name}</div>
+        <div className="company">{profile.company}</div>
       </div>
     </div>
-  );
-
-// Read input from the user
-const Form = function(state, props) {
-  state = { userName: '' };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const resp = await axios.get(`https://api.github.com/users/${state.userName}`);
+  )
+}
+const Form = function(props) {
+  const [userName, setUserName] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${userName}`);
     props.onSubmit(resp.data);
-    const [userNameInput, setUserNameInput] = useState();
-    state = {userName: {userNameInput}};
   };
+
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" onChange={(e) => {setUserNameInput(e.target.value)} placeholder="GitHub username" required />
+      <input 
+        type="text" 
+        onChange={(e) => {setUserName(e.target.value)}}
+        value={userName} 
+        placeholder="GitHub username" 
+        required
+      />
       <button>Add card</button>
     </form>
   );
-};
+}
 
-// Manage the relation between all the other components
-const App = function(props, state) {
-  state = {
-    profiles: [],
+const App = ({title}) => {
+  const state = {
+    profiles: testData,
+    // profiles: [],
   };
   const addNewProfile = (profileData) => {
-    // ToDo: setState -> useState 
-    setState = (prevState => ({
-      profiles: [...prevState.profiles, profileData]
-    }))
-    const [profileList, setprofileList] = useState([]);
-  };
+    console.log('App', profileData);
+  }
+  // const [myState, setMyState] = useState([]);
+
   return (
-    <div>
-      <div className="header">{props.title}</div>
+    <>
+      <div className="header">{title}</div>
       <Form onSubmit={addNewProfile} />
-      <CardList profiles={state.profiles} />
-    </div>
+      <CardList profiles={state.profiles}/>
+    </>
   );
-}
+};
+
+// class App extends React.Component {
+//   state = {
+//     profiles: [],
+//   };
+//   addNewProfile = (profileData) => {
+//     this.setState(prevState => ({
+//       profiles: [...prevState.profiles, profileData]
+//     }))
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <div className="header">{this.props.title}</div>
+//         <Form onSubmit={this.addNewProfile} />
+//         <CardList profiles={this.state.profiles} />
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(
   <App title="The GitHub Cards App" />,
