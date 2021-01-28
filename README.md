3f104cfd61e1bd35278fd1f9efdef85deeaf8d9c
Mrs. Zwickermann <azimmermann@epages.com>
2021-01-28 13:03:07 +0100

    New: React Hooks Tutorial build a to-do list

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # My Playground
 
-* [GitHub Cards App](https://github.com/azwick/playground/tree/master/github-cards-app)
-* [React Hooks Playground (Toolchain)](https://github.com/azwick/playground/tree/master/setting-up-toolchain)
+* [ToDo List](https://github.com/azwick/playground/tree/master/todo-list)
 * [Dashboard](https://github.com/azwick/playground/tree/master/dashboard)
+* [React Hooks Playground (Toolchain)](https://github.com/azwick/playground/tree/master/setting-up-toolchain)
+* [GitHub Cards App](https://github.com/azwick/playground/tree/master/github-cards-app)
---
+++ todo-list/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+//components
+import Header from "./Header";
+import ToDoForm from './ToDoForm';
+import ToDoList from "./ToDoList";
+//mock data
+import data from "./data.json";
+
+function App() {
+  
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
+    </div>
+  );
+}
+
+export default App;
