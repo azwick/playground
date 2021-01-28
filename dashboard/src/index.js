5f349debca67c4a0c8ef66ad95f399293282742d
Mrs. Zwickermann <azimmermann@epages.com>
2021-01-28 16:57:53 +0100

    New: Try to improve prio checklist

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 
-import Checklist from './Checklist';
+// import Checklist from './Checklist';
+import ChecklistNew from './ChecklistNew';
 import Clock from './Clock';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -12,9 +13,12 @@ const App = () => {
         <Clock/>
         <div>
           <h1>Prio Checkliste</h1>
-          <Checklist title='Prio A' maxCount={3} />
+          {/* <Checklist title='Prio A' maxCount={3} />
           <Checklist title='Prio B' maxCount={5} />
-          <Checklist title='Prio C' maxCount={7} />
+          <Checklist title='Prio C' maxCount={7} /> */}
+          <ChecklistNew title='Prio A' maxCount={5} />
+          <ChecklistNew title='Prio B' maxCount={6} />
+          <ChecklistNew title='Prio C' maxCount={7} />
         </div>
       </div>
     </div>
